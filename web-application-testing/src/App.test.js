b629d2773f5502fb35b2dd5aabfa34085ba9e5d7
Alberto Galvan <astrometrics@Unimatrix-Zero.local>
2019-10-31 20:43:25 -0500

    mvp complete

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -2,10 +2,18 @@ import React, { useState } from 'react';
 import { render, cleanup } from '@testing-library/react';
 import App from './App';
 
-
+afterEach(cleanup);
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  render(<App />);
+})
+
+test('There is ball btn', () => {
+  const { getByText } = render(<App />)
+  getByText(/Balls/i)
+})
+
+test('There is strike btn', () => {
+  const { getByText } = render(<App />)
+  getByText(/Strikes/i)
+})
